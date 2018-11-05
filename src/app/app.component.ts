8e98b2ed5d3d27f1203686af480a2f1a24f3a1cd
alexandrejuk <ale_santos.soares@hotmail.com>
2018-11-05 14:41:02 -0200

    component: create app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { EmployeeService } from './services/employee.service';
+import { Employee } from './models/employee';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  public employeeForm: FormGroup;
+  public employees$: Observable<Employee[]>;
+  public dataChar;
+
+  constructor(
+    private _fb: FormBuilder,
+    private employeeService: EmployeeService
+  ) {  }
+
+  ngOnInit() {
+    this.initForm();
+    this.getEmployees();
+  }
+
+  initForm() {
+    this.employeeForm = this._fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      participation: ['', Validators.required],
+    });
+  }
+
+  getEmployees() {
+    this.employees$ = this.employeeService.employees.pipe(map(employees => {
+      this.dataChar = employees.map(
+        employee => ({
+          name: employee.firstName,
+          value: employee.participation
+        }));
+      return employees;
+    }));
+    this.employeeService.getAllEmployees();
+  }
+
+  saveEmployee(e) {
+    this.employeeService.addEmployee(this.employeeForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
 import { AppComponent } from './app.component';
+import { EmployeeService } from './services/employee.service';
+import { PieChartComponent } from './components/pie-chart/pie-chart.component';
+import { TableComponent } from './components/table/table.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieChartComponent,
+    TableComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    NgxChartsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
